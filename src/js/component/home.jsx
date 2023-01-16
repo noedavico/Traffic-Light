2ed36a1288b7ebf135a1bcb15b3cf366c06a4657
noedavico <noedavico@hotmail.com>
2023-01-16 23:13:18 +0000

    parcial

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
 import React from "react";
-
+import { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+const TrafficLight = () => {
+	const [color, setColor] = useState("");
+    const [index, setIndex] = useState(0);
+	const colorLight = ["red", "yellow", "green"];
+	const [opened, setOpened] = useState(true);
 
-export default Home;
+	const manual = () => {
+		setIndex(index+1)
+		setColor(colorLight[index]);
+		if (index >= 2) {
+			setIndex (0)	
+		}
+	}
+
+    const automatic = () => {
+		let index = 0;
+		const colorLight = ["red", "yellow", "green"];
+		setInterval(() => {
+		  if (index <= colorLight.length) {
+			setColor(colorLight[index]);
+			index++;
+		  } else {
+			return false;
+		  }
+		}, 500);
+	  };
+
+	return ( 
+	<>
+	   <div className="container">
+	   <div className="box"></div>
+	   <div  id="trafficlights">
+		   <div className={"red " + (color === "red" ? "brillo" : "")} onClick={() => setColor("red")}></div>
+		   <div className={"yellow " + (color === "yellow" ? "brillo" : "")} onClick={() => setColor("yellow")}></div>
+		   <div className={"green " + (color === "green" ? "brillo" : "")} onClick={() => setColor("green")}></div>
+		   <div className={"purple " + (color === "purple" ? "brillo" : "")} onClick={() => setColor("purple")}></div>
+      
+		</div>
+		<div className="d-grid  col-6 mx-auto">
+		<button className="btn btn-outline-dark m-3" onClick={manual}>
+		   Hand cycle
+        </button>
+		<button className="btn btn-outline-dark m-3 " onClick={automatic}>
+		   Automatic cycle
+        </button>
+		</div>
+		</div>
+		</>
+	);
+  };
+
+export default TrafficLight;
