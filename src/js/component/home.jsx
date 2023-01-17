4a16a2063d528ab8c255b2c33f444dc2e85a7b90
noedavico <noedavico@hotmail.com>
2023-01-17 15:25:20 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,11 +4,16 @@ import { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 const TrafficLight = () => {
+	/*Estado para asignarle el brillo*/
 	const [color, setColor] = useState("");
+	/*Estado para actualizar el index al hacer clik */
     const [index, setIndex] = useState(0);
+	/*array colores*/
 	const colorLight = ["red", "yellow", "green"];
-	const [opened, setOpened] = useState(true);
+	/*Estado para mostrar u ocultar color purpura*/
+	const [mostrarComponente, setMostrarComponente] = useState(true);
 
+	/*Funcion para cambiar el encedido de semaforo de manera manual al clickear el boton*/
 	const manual = () => {
 		setIndex(index+1)
 		setColor(colorLight[index]);
@@ -16,7 +21,7 @@ const TrafficLight = () => {
 			setIndex (0)	
 		}
 	}
-
+    /*Funcion al hacer click en el boton camabiara de luz de manera automatica al cada 5seg*/
     const automatic = () => {
 		let index = 0;
 		const colorLight = ["red", "yellow", "green"];
@@ -38,13 +43,20 @@ const TrafficLight = () => {
 		   <div className={"red " + (color === "red" ? "brillo" : "")} onClick={() => setColor("red")}></div>
 		   <div className={"yellow " + (color === "yellow" ? "brillo" : "")} onClick={() => setColor("yellow")}></div>
 		   <div className={"green " + (color === "green" ? "brillo" : "")} onClick={() => setColor("green")}></div>
-		   <div className={"purple " + (color === "purple" ? "brillo" : "")} onClick={() => setColor("purple")}></div>
-      
+		   <div className={mostrarComponente ? "show-element" : null}>
+           {mostrarComponente && <div className={"purple " + (color === "purple" ? "brillo" : "")} onClick={() => setColor("purple")}></div>}
+        </div>   
 		</div>
-		<div className="d-grid  col-6 mx-auto">
+		<div className="d-grid  col-4 mx-auto">
+		{/*Con un estado adicional le dicen cuando mostrarse o no*/}
+		<button className="btn btn-outline-dark m-3" onClick={() => setMostrarComponente(!mostrarComponente)}>
+        {/*Aqui solo cambio el texto de mi boton */}
+        {mostrarComponente ? `Purple off ` : `Purple on`} </button>
+		{/*Boton asociado a la funcion manual*/}
 		<button className="btn btn-outline-dark m-3" onClick={manual}>
 		   Hand cycle
         </button>
+		{/*Boton asociado a la funcion automatica*/}
 		<button className="btn btn-outline-dark m-3 " onClick={automatic}>
 		   Automatic cycle
         </button>
